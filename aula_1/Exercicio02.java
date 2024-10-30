2f67801c9b6dc875d120061f113386a364c5d6fb
GustavoEmiliano <gustavoemiliano2014@gmail.com>
2024-10-30 17:57:43 -0300

    add: init repository fiap - java oo

---
+++ aula_1/Exercicio02.java
@@ -0,0 +1,13 @@
+package aula_1;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            soma += i;
+        }
+
+        System.out.println("A soma dos números de 0 a 100 é: " + soma);
+    }
+}
